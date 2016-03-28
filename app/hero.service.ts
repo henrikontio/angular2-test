da23d62c243a05862a7241dd46ced716434cd8d4
Henri Kontio <Henri.Kontio@solita.fi>
2016-03-29 00:39:00 +0300

    Use hero service to fetch data

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,4 +1,9 @@
 import {Injectable} from 'angular2/core';
+import {HEROES} from './mock-heroes';
 
 @Injectable()
-export class HeroService {}
+export class HeroService {
+  getHeroes() {
+    return Promise.resolve(HEROES);
+  }
+}
